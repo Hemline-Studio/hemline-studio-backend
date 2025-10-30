8edefc646cb4fdf83bf5a6229c547d9a8ab107d0
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2025-10-30 08:30:05 +0100

    fix: Update queue schema loading command in render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -10,4 +10,4 @@ bundle exec rails db:create
 bundle exec rails db:migrate
 
 # Load Solid Queue schema to create background job tables
-bundle exec rails db:schema:load:queue
+bundle exec rails runner "load 'db/queue_schema.rb'"
