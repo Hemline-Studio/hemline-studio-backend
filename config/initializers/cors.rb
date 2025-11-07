86c77469ac12fd6f1a5ff993e26af925d25e6d3c
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2025-11-07 19:21:17 +0100

    fix: Simplify CORS configuration to allow all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,27 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # Allow specific origins based on environment
-    if Rails.env.production?
-      origins "https://hemline-frontend.vercel.app",
-              "https://hemline.studio",
-              "https://www.hemline.studio",
-              /https:\/\/.*\.vercel\.app$/  # Allow all Vercel preview deployments
-    else
-      # In development, allow localhost with different ports
-      origins "http://localhost:3000",
-              "http://localhost:3001",
-              "http://localhost:5173",
-              "http://127.0.0.1:3000",
-              "http://127.0.0.1:3001",
-              "http://127.0.0.1:5173"
-    end
+    origins "*"
 
     resource "*",
       headers: :any,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
-      credentials: false,
-      expose: [ "Authorization" ],
-      max_age: 600
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
